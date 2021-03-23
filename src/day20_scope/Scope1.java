5b32b62cba16380147fe6375844065b138a4834a
Aziz <azizekeathn@gmail.com>
2021-03-24 00:53:44 +0200

    day34

--- src/day20_scope/Scope1.java
+++ src/day20_scope/Scope1.java
@@ -3,20 +3,22 @@ package day20_scope;
 import java.util.Scanner;
 
 public class Scope1 {
-			 int sayi; // class'in icinde fakat main method'un disinda olusturulan
-		     // ve static olmayan variable'lara INSTANCE (Object) variable denir
-		     // class level'da olusturuldugu icin class'in her yerinden belli sartlarla kullanilabilir
-		     // Instance variable'lar object'e bagli olarak calisir (ogretmen adi veya, ogrenci notu gibi)
+			public int sayi; // class'in icinde fakat main method'un disinda olusturulan
+			  // ve static olmayan variable'lara INSTANCE (Object) variable denir
+		    // class level'da olusturuldugu icin class'in her yerinden belli sartlarla kullanilabilir
+			  // Instance variable'lar object'e bagli olarak calisir (ogretmen adi veya, ogrenci notu gibi)
 		public String isim="Mehmet";
-		String soyisim; // default deger null
+		public String soyisim; // default deger null
 		boolean izindeMi; // deafult deger false
 		char ilkHarf; // default deger bosluk
+		
 		
 		public static void main(String[] args) {
 		
 		// System.out.println(sayi); // static olmayan bir variable'a static method icinden ulasamayiz
-		                       // main method'umuz static oldugu icin main method icerisinden 
-		//method();             // static olmayan variable veya method'lara direk ulasamayiz
+								// main method'umuz static oldugu icin main method icerisinden 
+		//method();				// static olmayan variable veya method'lara direk ulasamayiz
+		
 		// instance bir variable'a main method icerisinden ulasmak istedigimizde 
 		// OBJECT olusturmaliyiz
 		
@@ -47,7 +49,6 @@ public class Scope1 {
 		System.out.println(obj3.isim +" " +  obj3.soyisim+  " " + obj3.izindeMi ); // Ferdi Tayfur true
 		
 		scan.close();
-		
 		}
 		
 		public static void staticMethod() {
@@ -56,8 +57,8 @@ public class Scope1 {
 		
 		
 		public void method() {
-		
-		
-			}
+		System.out.println(sayi); // bu method static olmadigi icin instance variable'lara direk erisebilir
+		sayi=sayi+20;  // ve bu method iceriisinde gecerli olmak uzere degerini degistirebilir 
+					}
 
 }
--- src/day21_scope/Scope6.java
+++ src/day21_scope/Scope6.java
@@ -2,24 +2,30 @@ package day21_scope;
 
 public class Scope6 {
 
+	public int num1;	 // access modifier public yapilinca heryerden ulasilabilir
+	String name = "Ali"; // access modifier yazilmadigi icin default access modifier gecerlidir
+						 // sadece paket icinde kullanilabilir
 	
-		int num1;
-		String name = "Ali";
-		public static void main(String args){ 
-		  add();
-		//product (5);//static olmayan bir methodu static method icerisinden cagrilamaz
-		}
-		public static void add(){
-		//num1 ++; ==> burda da num1 instance veriable ve static olmadigi icin static methodun iicnden cagrimaya 
-			//calsilmis ondan dolayi cte verir
-		int num2 = 6;
-		char letter;
-		System.out.println("Do addition ");
-		}
-		public void product(int num3){
-		name = "Veli";
-		//num2++;  ==> num2 farkli bir methoda olurutulmus ve baska bir methodun iicnden cagrilamaz.
-		System.out.println(num3 * num3);
+	public static void main(String args){ 
+	  add();
+	// product (5); static olmayan bir method static olan main method icerisinden cagrilamaz
+	}
+	
+	public static void add(){
+	// num1++; // static olmayan (instance) num1, static bir method icinden kullanilamaz
+	int num2 = 6;
+	char letter;
+	System.out.println("Do addition ");
+	
+	letter='a';
+	System.out.println(num2 + " " + letter);
+	}
+	
+	
+	public void product(int num3){
+	name = "Veli";
+	 // num2++; // num2 ustteki methodda olusturulmus Local bir variable'dir. 
+	System.out.println(num3 * num3);
 		} 
 
 	}
