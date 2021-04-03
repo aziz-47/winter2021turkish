591fc4f8cd38d29c1fe0c9befaa198f30d560a9b
Aziz <azizekeathn@gmail.com>
2021-04-03 17:02:24 +0300

    day42

--- src/day40_exceptions/Exceptions04.java
+++ src/day40_exceptions/Exceptions04.java
@@ -3,8 +3,29 @@ package day40_exceptions;
 public class Exceptions04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+        System.out.print(exceptions());
+    }
+    @SuppressWarnings("finally")
+    public static String exceptions() {
+        String result = "";
+        String v = null;
+        try {
+            try {
+                result = result + "a";
+                v.length();
+                result = result + "b";
+            } catch (NullPointerException e) {
+                result = result + "c";
+            } finally {
+                result = result + "d";
+                throw new Exception();
+            
+            }
+        } catch (Exception e) {
+            result = result + "e";
+        }
+        return result;
 	}
 
 }
--- src/day40_exceptions/InvalidEmailIdCheckedException.java
+++ src/day40_exceptions/InvalidEmailIdCheckedException.java
@@ -1,5 +1,11 @@
 package day40_exceptions;
 
-public class InvalidEmailIdCheckedException {
+public class InvalidEmailIdCheckedException extends Exception{
+	
+	 private static final long serialVersionUID = 1L; 
+	    // Java exception'larin tekrarsiz olmasini saglamak icin her exception'a unique bir kod verir
+	    InvalidEmailIdCheckedException(String message){
+	        super(message);
+	}
 
 }
