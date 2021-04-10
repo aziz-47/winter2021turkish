1e9cdc7cae112c3eb008f56940a7e20ffcd1a3f6
Aziz <azizekeathn@gmail.com>
2021-04-10 23:23:12 +0300

    mutlu son

---
+++ src/day49_maps/Maps04_VeriTabani.java
@@ -0,0 +1,42 @@
+package day49_maps;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps04_VeriTabani {
+
+	public static void main(String[] args) {
+       String dosyaYolu = "C:\\Users\\Dell\\Desktop\\Veritabani.csv";
+       
+       Map<String,String> veriMap = veriTabaniAl(dosyaYolu);
+       System.out.println(veriMap);
+       
+   }
+   public static Map<String, String> veriTabaniAl(String dosyaYolu)  {
+       
+       Map<String,String> eklenecekMap =new HashMap<>();
+       
+           try {
+               BufferedReader br=new BufferedReader(new FileReader(dosyaYolu));
+               String satir=br.readLine();
+               
+               
+               while(satir != null) {
+                   
+                   String keyValueArr[]=satir.split(",");
+                   eklenecekMap.put(keyValueArr[0], keyValueArr[1]);
+                   satir=br.readLine();
+               }               
+           } catch ( IOException e) {
+               
+               e.printStackTrace(); // detayli hata raporu verir
+           }
+       
+       return eklenecekMap;
+	}
+
+}
